caf4b77c0b6e2a107ecab06a5b818ab4ddb66cb0
Clara Castillo <claracastillochale@gmail.com>
2022-09-18 13:22:58 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-pokedex';
-}
+export class AppComponent {}
